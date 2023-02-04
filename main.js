b1667c3478b0a934a138a24f681356083fe2d205
Aditi-panta <88805173+Aditi-panta@users.noreply.github.com>
2023-02-04 13:36:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+    background("white");
+}
+function clearCanvas(){
+    background("white");
+}
+function preload(){
+    classifier=ml5.imageClassifier("DoodleNet");
+}
